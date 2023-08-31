68260d9b1c96a9a84d31b7fc689ea0ac0e48300a
Elfego <eapen.b@outlook.es>
2023-08-30 20:04:59 -0600

    Se agrego la validación de cuenta activa, y una tabla para visualizar los datos

--- partidasContables/Form1.Designer.cs
+++ partidasContables/Form1.Designer.cs
@@ -42,17 +42,21 @@
             label4 = new Label();
             btnGuardar = new Button();
             lblMesOp = new Label();
-            label6 = new Label();
+            lblAnioOp = new Label();
             lblEstadoOp = new Label();
             txtDescripcion = new TextBox();
             lblDescripcion = new Label();
+            dataGridViewPartidas = new DataGridView();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPartidas).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(47, 75);
+            label1.Location = new Point(47, 85);
             label1.Name = "label1";
             label1.Size = new Size(102, 21);
             label1.TabIndex = 0;
@@ -62,7 +66,7 @@
             // 
             lblOperacion.AutoSize = true;
             lblOperacion.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            lblOperacion.Location = new Point(47, 22);
+            lblOperacion.Location = new Point(47, 25);
             lblOperacion.Name = "lblOperacion";
             lblOperacion.Size = new Size(163, 21);
             lblOperacion.TabIndex = 1;
@@ -74,17 +78,17 @@
             fechaPartida.CalendarFont = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             fechaPartida.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             fechaPartida.Format = DateTimePickerFormat.Short;
-            fechaPartida.Location = new Point(155, 74);
+            fechaPartida.Location = new Point(155, 84);
             fechaPartida.Name = "fechaPartida";
             fechaPartida.RightToLeft = RightToLeft.No;
-            fechaPartida.Size = new Size(432, 29);
+            fechaPartida.Size = new Size(578, 29);
             fechaPartida.TabIndex = 2;
             // 
             // lblMesOperacion
             // 
             lblMesOperacion.AutoSize = true;
             lblMesOperacion.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            lblMesOperacion.Location = new Point(217, 22);
+            lblMesOperacion.Location = new Point(217, 25);
             lblMesOperacion.Name = "lblMesOperacion";
             lblMesOperacion.Size = new Size(42, 21);
             lblMesOperacion.TabIndex = 3;
@@ -95,7 +99,7 @@
             // 
             lblAnioOperacion.AutoSize = true;
             lblAnioOperacion.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            lblAnioOperacion.Location = new Point(336, 23);
+            lblAnioOperacion.Location = new Point(336, 26);
             lblAnioOperacion.Name = "lblAnioOperacion";
             lblAnioOperacion.Size = new Size(41, 21);
             lblAnioOperacion.TabIndex = 4;
@@ -106,7 +110,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(47, 125);
+            label2.Location = new Point(47, 142);
             label2.Name = "label2";
             label2.Size = new Size(59, 21);
             label2.TabIndex = 5;
@@ -115,16 +119,17 @@
             // txtNumeroCuenta
             // 
             txtNumeroCuenta.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtNumeroCuenta.Location = new Point(154, 122);
+            txtNumeroCuenta.Location = new Point(154, 138);
             txtNumeroCuenta.Name = "txtNumeroCuenta";
-            txtNumeroCuenta.Size = new Size(433, 29);
+            txtNumeroCuenta.Size = new Size(579, 29);
             txtNumeroCuenta.TabIndex = 6;
+            txtNumeroCuenta.KeyPress += txtNumeroCuenta_KeyPress;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(50, 171);
+            label3.Location = new Point(50, 194);
             label3.Name = "label3";
             label3.Size = new Size(56, 21);
             label3.TabIndex = 7;
@@ -133,16 +138,17 @@
             // txtMonto
             // 
             txtMonto.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtMonto.Location = new Point(154, 171);
+            txtMonto.Location = new Point(154, 194);
             txtMonto.Name = "txtMonto";
-            txtMonto.Size = new Size(433, 29);
+            txtMonto.Size = new Size(579, 29);
             txtMonto.TabIndex = 8;
+            txtMonto.KeyPress += txtMonto_KeyPress;
             // 
             // lblTipo
             // 
             lblTipo.AutoSize = true;
             lblTipo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            lblTipo.Location = new Point(47, 226);
+            lblTipo.Location = new Point(47, 256);
             lblTipo.Name = "lblTipo";
             lblTipo.Size = new Size(93, 21);
             lblTipo.TabIndex = 9;
@@ -153,16 +159,16 @@
             cmbTipoCuenta.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             cmbTipoCuenta.FormattingEnabled = true;
             cmbTipoCuenta.Items.AddRange(new object[] { "DEBE", "HABER" });
-            cmbTipoCuenta.Location = new Point(155, 223);
+            cmbTipoCuenta.Location = new Point(155, 253);
             cmbTipoCuenta.Name = "cmbTipoCuenta";
-            cmbTipoCuenta.Size = new Size(430, 29);
+            cmbTipoCuenta.Size = new Size(576, 29);
             cmbTipoCuenta.TabIndex = 10;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(444, 23);
+            label4.Location = new Point(444, 26);
             label4.Name = "label4";
             label4.Size = new Size(59, 21);
             label4.TabIndex = 11;
@@ -174,9 +180,9 @@
             btnGuardar.FlatAppearance.BorderColor = Color.MintCream;
             btnGuardar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnGuardar.ForeColor = SystemColors.ButtonHighlight;
-            btnGuardar.Location = new Point(438, 372);
+            btnGuardar.Location = new Point(468, 368);
             btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(147, 41);
+            btnGuardar.Size = new Size(262, 46);
             btnGuardar.TabIndex = 13;
             btnGuardar.Text = "GuardarPartida";
             btnGuardar.UseVisualStyleBackColor = false;
@@ -187,29 +193,29 @@
             lblMesOp.AutoSize = true;
             lblMesOp.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             lblMesOp.ForeColor = Color.Teal;
-            lblMesOp.Location = new Point(259, 23);
+            lblMesOp.Location = new Point(259, 26);
             lblMesOp.Name = "lblMesOp";
             lblMesOp.Size = new Size(73, 21);
             lblMesOp.TabIndex = 14;
             lblMesOp.Text = "AGOSTO";
             // 
-            // label6
+            // lblAnioOp
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label6.ForeColor = Color.Teal;
-            label6.Location = new Point(380, 23);
-            label6.Name = "label6";
-            label6.Size = new Size(46, 21);
-            label6.TabIndex = 15;
-            label6.Text = "2023";
+            lblAnioOp.AutoSize = true;
+            lblAnioOp.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblAnioOp.ForeColor = Color.Teal;
+            lblAnioOp.Location = new Point(380, 26);
+            lblAnioOp.Name = "lblAnioOp";
+            lblAnioOp.Size = new Size(46, 21);
+            lblAnioOp.TabIndex = 15;
+            lblAnioOp.Text = "2023";
             // 
             // lblEstadoOp
             // 
             lblEstadoOp.AutoSize = true;
             lblEstadoOp.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             lblEstadoOp.ForeColor = Color.FromArgb(0, 192, 0);
-            lblEstadoOp.Location = new Point(509, 23);
+            lblEstadoOp.Location = new Point(509, 26);
             lblEstadoOp.Name = "lblEstadoOp";
             lblEstadoOp.Size = new Size(75, 21);
             lblEstadoOp.TabIndex = 16;
@@ -218,30 +224,49 @@
             // txtDescripcion
             // 
             txtDescripcion.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            txtDescripcion.Location = new Point(154, 274);
+            txtDescripcion.Location = new Point(154, 311);
             txtDescripcion.Name = "txtDescripcion";
-            txtDescripcion.Size = new Size(433, 29);
+            txtDescripcion.Size = new Size(579, 29);
             txtDescripcion.TabIndex = 18;
             // 
             // lblDescripcion
             // 
             lblDescripcion.AutoSize = true;
             lblDescripcion.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            lblDescripcion.Location = new Point(50, 274);
+            lblDescripcion.Location = new Point(50, 311);
             lblDescripcion.Name = "lblDescripcion";
             lblDescripcion.Size = new Size(91, 21);
             lblDescripcion.TabIndex = 17;
             lblDescripcion.Text = "Descripción";
             // 
+            // dataGridViewPartidas
+            // 
+            dataGridViewPartidas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewPartidas.Dock = DockStyle.Fill;
+            dataGridViewPartidas.Location = new Point(0, 0);
+            dataGridViewPartidas.Name = "dataGridViewPartidas";
+            dataGridViewPartidas.RowTemplate.Height = 27;
+            dataGridViewPartidas.Size = new Size(749, 314);
+            dataGridViewPartidas.TabIndex = 19;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(dataGridViewPartidas);
+            panel1.Location = new Point(12, 443);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(749, 314);
+            panel1.TabIndex = 20;
+            // 
             // formularioPartida
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(627, 465);
+            ClientSize = new Size(772, 769);
+            Controls.Add(panel1);
             Controls.Add(txtDescripcion);
             Controls.Add(lblDescripcion);
             Controls.Add(lblEstadoOp);
-            Controls.Add(label6);
+            Controls.Add(lblAnioOp);
             Controls.Add(lblMesOp);
             Controls.Add(btnGuardar);
             Controls.Add(label4);
@@ -259,6 +284,8 @@
             Name = "formularioPartida";
             Text = "Partidas Contables";
             Load += formularioPartida_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPartidas).EndInit();
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -279,9 +306,11 @@
         private Label label4;
         private Button btnGuardar;
         private Label lblMesOp;
-        private Label label6;
+        private Label lblAnioOp;
         private Label lblEstadoOp;
         private TextBox txtDescripcion;
         private Label lblDescripcion;
+        private DataGridView dataGridViewPartidas;
+        private Panel panel1;
     }
 }
--- partidasContables/Form1.cs
+++ partidasContables/Form1.cs
@@ -2,9 +2,68 @@ namespace partidasContables
 {
     public partial class formularioPartida : Form
     {
+        int correlativo = 0;
+        struct CuentaContable
+        {
+            public int NumeroCuenta { get; set; }
+            public string NombreCuenta { get; set; }
+            public string EstadoCuenta { get; set; }
+        }
+
+        private CuentaContable[] ct = new CuentaContable[]
+        {
+            new CuentaContable { NumeroCuenta = 123456, NombreCuenta = "COMBUSTIBLES Y LUBRICANTES", EstadoCuenta = "ACTIVO"},
+            new CuentaContable { NumeroCuenta = 654321, NombreCuenta = "PAPELERIA Y UTILES", EstadoCuenta = "ACTIVO"},
+            new CuentaContable { NumeroCuenta = 111111, NombreCuenta = "ATENCION AL PERSONAL", EstadoCuenta = "INACTIVO"},
+        };
+        struct Mes
+        {
+            public int Numero { get; set; }
+            public string Nombre { get; set; }
+        }
+
+        private Mes[] meses = new Mes[]
+        {
+            new Mes { Numero = 1, Nombre = "Enero" },
+            new Mes { Numero = 2, Nombre = "Febrero" },
+            new Mes { Numero = 3, Nombre = "Marzo" },
+            new Mes { Numero = 4, Nombre = "Abril" },
+            new Mes { Numero = 5, Nombre = "Mayo" },
+            new Mes { Numero = 6, Nombre = "Junio" },
+            new Mes { Numero = 7, Nombre = "Julio" },
+            new Mes { Numero = 8, Nombre = "Agosto" },
+            new Mes { Numero = 9, Nombre = "Septiembre" },
+            new Mes { Numero = 10, Nombre = "Octubre" },
+            new Mes { Numero = 11, Nombre = "Noviembre" },
+            new Mes { Numero = 12, Nombre = "Diciembre" }
+        };
+
+        struct Partida
+        {
+            public int CorrPartida { get; set; }
+            public int CorrMes { get; set; }
+            public string FechaPartida { get; set; }
+            public int NumeroCta { get; set; }
+            public decimal CantidadCuenta { get; set; }
+            public string TipoCuenta { get; set; }
+            public string DescripcionPartida { get; set; }
+        }
+
+        private List<Partida> partidasList = new List<Partida>();
         public formularioPartida()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            if (lblEstadoOp.Text == "CERRADO")
+            {
+
+                fechaPartida.Enabled = false;
+                txtNumeroCuenta.Enabled = false;
+                txtMonto.Enabled = false;
+                cmbTipoCuenta.Enabled = false;
+                txtDescripcion.Enabled = false;
+                btnGuardar.Enabled = false;
+            }
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -29,11 +88,17 @@ namespace partidasContables
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            String fecha = fechaPartida.Text;
+
+            String fecha = fechaPartida.Value.ToString("dd/MM/yyyy");
             String cuenta = txtNumeroCuenta.Text;
             String cantidad = txtMonto.Text;
             String tipoCuenta = cmbTipoCuenta.Text;
             String descripcion = txtDescripcion.Text;
+
+            int cuentaCT = int.Parse(txtNumeroCuenta.Text);
+            decimal montoPartida = decimal.Parse(txtMonto.Text);
+
+
 
             if (fecha == "" || cuenta == "" || cantidad == "" || tipoCuenta == "" || descripcion == "")
             {
@@ -74,10 +139,218 @@ namespace partidasContables
             }
             else
             {
-                MessageBox.Show(fecha + cuenta + cantidad + tipoCuenta + descripcion);
+                if (ObtenerEstadoCuenta(Int32.Parse(cuenta)) == "ACTIVO")
+                {
+                    if (validaEstadoMes())
+                    {
+                        MessageBox.Show("El mes esta activo");
+                        MessageBox.Show(fecha + cuenta + cantidad + tipoCuenta + descripcion);
+                        int anio = ExtraerAniodeFecha(fecha);
+                        int mesPartida = ExtraerMesdeFecha(fecha);
+
+                        String mesDigitado = ObtenerNombreMes(mesPartida).ToUpper();
+
+                        if (mesDigitado == lblMesOp.Text && anio == Int32.Parse(lblAnioOp.Text))
+                        {
+                            correlativo++;
+                            AgregarPartida(correlativo, mesPartida, fecha, cuentaCT, montoPartida, tipoCuenta, descripcion);
+                            MessageBox.Show("estos es" + correlativo + mesPartida + fecha + cuentaCT + montoPartida + tipoCuenta + descripcion);
+                            InitializeDataGridView();
+                            FillDataGridView();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Estas tratando de ingresar información en un mes diferente" +
+                                ", el mes del periodo es: " + lblMesOp.Text);
+                        }
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("El mes esta cerrado, comuniquese al departamento de informatica");
+
+                        fechaPartida.Enabled = false;
+                        txtNumeroCuenta.Enabled = false;
+                        txtMonto.Enabled = false;
+                        cmbTipoCuenta.Enabled = false;
+                        txtDescripcion.Enabled = false;
+
+                    }
+
+
+                }
+                else
+                {
+                    MessageBox.Show("El número de cuenta: " + cuenta + ", esta inactivo");
+                }
+
+
             }
 
 
         }
+
+        private void txtNumeroCuenta_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtMonto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //MessageBox.Show(Convert.ToInt16(e.KeyChar).ToString());
+
+            if (e.KeyChar == 8)
+            {
+                e.Handled = false;
+                return;
+            }
+
+
+            bool IsDec = false;
+            int nroDec = 0;
+
+            for (int i = 0; i < txtMonto.Text.Length; i++)
+            {
+                if (txtMonto.Text[i] == '.')
+                    IsDec = true;
+
+                if (IsDec && nroDec++ >= 2)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+
+            }
+
+            if (e.KeyChar >= 48 && e.KeyChar <= 57)
+                e.Handled = false;
+            else if (e.KeyChar == 46)
+                e.Handled = (IsDec) ? true : false;
+            else
+                e.Handled = true;
+
+        }
+
+        private void generarPoliza()
+        {
+        }
+
+        private void validarMes(String fecha)
+        {
+
+
+        }
+
+        bool validaEstadoMes()
+        {
+            String estadoMes = lblEstadoOp.Text.ToUpper();
+            bool estado;
+            if (estadoMes == "ABIERTO")
+            {
+                estado = true;
+            }
+            else
+            {
+                estado = false;
+            }
+            return estado;
+        }
+        private int ExtraerMesdeFecha(string date)
+        {
+            DateTime parsedDate;
+            if (DateTime.TryParseExact(date, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out parsedDate))
+            {
+                return parsedDate.Month;
+            }
+            else
+            {
+                throw new ArgumentException("Fecha no válida en el formato dd/mm/yyyy", nameof(date));
+            }
+        }
+
+        private int ExtraerAniodeFecha(string date)
+        {
+            DateTime parsedDate;
+            if (DateTime.TryParseExact(date, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out parsedDate))
+            {
+                return parsedDate.Year;
+            }
+            else
+            {
+                throw new ArgumentException("Fecha no válida en el formato dd/mm/yyyy", nameof(date));
+            }
+        }
+
+        private string ObtenerNombreMes(int monthNumber)
+        {
+            foreach (Mes mes in meses)
+            {
+                if (mes.Numero == monthNumber)
+                {
+                    return mes.Nombre;
+                }
+            }
+            return "Mes no encontrado";
+        }
+
+        private string ObtenerEstadoCuenta(int accountNumber)
+        {
+            foreach (CuentaContable cuenta in ct)
+            {
+                if (cuenta.NumeroCuenta == accountNumber)
+                {
+                    return cuenta.EstadoCuenta;
+                }
+            }
+            return "Cuenta no encontrada";
+        }
+
+        private void AgregarPartida(int corrPartida, int corrMes, string fechaPartida,
+                                   int numeroCta, decimal cantidadCuenta, string tipoCuenta, string descripcionPartida)
+        {
+            Partida nuevaPartida = new Partida
+            {
+                CorrPartida = corrPartida,
+                CorrMes = corrMes,
+                FechaPartida = fechaPartida,
+                NumeroCta = numeroCta,
+                CantidadCuenta = cantidadCuenta,
+                TipoCuenta = tipoCuenta,
+                DescripcionPartida = descripcionPartida
+            };
+
+            partidasList.Add(nuevaPartida);
+        }
+
+        private void FillDataGridView()
+        {
+            dataGridViewPartidas.Rows.Clear();
+
+            foreach (Partida partida in partidasList)
+            {
+                dataGridViewPartidas.Rows.Add(partida.CorrPartida, partida.CorrMes, partida.FechaPartida, partida.NumeroCta,
+                    partida.CantidadCuenta, partida.TipoCuenta, partida.DescripcionPartida);
+            }
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridViewPartidas.ColumnCount = 7; // Cambia esto al número correcto de columnas
+
+            dataGridViewPartidas.Columns[0].Name = "No.";
+            dataGridViewPartidas.Columns[1].Name = "No.Mes";
+            dataGridViewPartidas.Columns[2].Name = "Fecha";
+            dataGridViewPartidas.Columns[3].Name = "No.Cuenta";
+            dataGridViewPartidas.Columns[4].Name = "Monto";
+            dataGridViewPartidas.Columns[5].Name = "Tipo";
+            dataGridViewPartidas.Columns[6].Name = "Descripción";
+        }
+
+
     }
 }
